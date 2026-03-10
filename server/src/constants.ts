d415fdfd245e47812802db7b87567907a4fc645d
emondsarker <emon331@gmail.com>
2026-03-10 16:51:27 -0700

    fix: filepath

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -5,4 +5,4 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const PORT: number = 4000;
-export const DATA_DIR: string = path.resolve(__dirname, "../data");
+export const DATA_DIR: string = path.resolve(__dirname, "./data");
